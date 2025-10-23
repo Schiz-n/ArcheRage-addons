feeb60c6ef7f62fe8c4df41ace93285a6986e2dd
Schiz-n <Noiryxp@hotmail.com>
2025-10-23 12:07:05 +0200

    petrebinder: move BindingToOption to only be ran once
    seems to lower the frame drop issue

--- z_pub_wip/petrebinder.lua
+++ z_pub_wip/petrebinder.lua
@@ -29,23 +29,23 @@ ADDON:ImportAPI(API_TYPE.HOTKEY.id)
 --⠿⠿⠿⠿⠿⠦⠈⠀⠀⠀⠸⠀⠀⠀⠀
 --what a shame⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 
+        X2Hotkey:BindingToOption()
 function SpawnPet(info1, info2, info3, info4, info5, info6)
     if (info1 == 1) then
-    	X2Hotkey:BindingToOption()
-    	aaprint(X2Unit:UnitName("playerpet1"))
-    	if X2Unit:UnitName("playerpet1") == "Strawberrykirin" then
-    		X2Hotkey:SetOptionBindingWithIndex("ride_pet_action_bar_button","CTRL-b", 1, 1)
-    	elseif X2Unit:UnitName("playerpet1") == "Strawberrydeer" then
-    		X2Hotkey:SetOptionBindingWithIndex("ride_pet_action_bar_button","CTRL-b", 1, 2)
-    	elseif X2Unit:UnitName("playerpet1") == "Strawberryleo" then
-    		X2Hotkey:SetOptionBindingWithIndex("ride_pet_action_bar_button","CTRL-b", 1, 2)
-    	elseif X2Unit:UnitName("playerpet1") == "Strawberrysteel" then
-    		X2Hotkey:SetOptionBindingWithIndex("ride_pet_action_bar_button","CTRL-b", 1, 1)
-    	elseif X2Unit:UnitName("playerpet1") == "Strawberrytaurus" then
-    		X2Hotkey:SetOptionBindingWithIndex("ride_pet_action_bar_button","CTRL-b", 1, 1)
-    	else
-    		X2Hotkey:SetOptionBindingWithIndex("ride_pet_action_bar_button","CTRL-b", 1, 1)
-    	end
+        aaprint(X2Unit:UnitName("playerpet1"))
+        if X2Unit:UnitName("playerpet1") == "Strawberrykirin" then
+            X2Hotkey:SetOptionBindingWithIndex("ride_pet_action_bar_button","CTRL-b", 1, 1)
+        elseif X2Unit:UnitName("playerpet1") == "Strawberrydeer" then
+            X2Hotkey:SetOptionBindingWithIndex("ride_pet_action_bar_button","CTRL-b", 1, 2)
+        elseif X2Unit:UnitName("playerpet1") == "Strawberryleo" then
+            X2Hotkey:SetOptionBindingWithIndex("ride_pet_action_bar_button","CTRL-b", 1, 2)
+        elseif X2Unit:UnitName("playerpet1") == "Strawberrysteel" then
+            X2Hotkey:SetOptionBindingWithIndex("ride_pet_action_bar_button","CTRL-b", 1, 1)
+        elseif X2Unit:UnitName("playerpet1") == "Strawberrytaurus" then
+            X2Hotkey:SetOptionBindingWithIndex("ride_pet_action_bar_button","CTRL-b", 1, 1)
+        else
+            X2Hotkey:SetOptionBindingWithIndex("ride_pet_action_bar_button","CTRL-b", 1, 1)
+        end
         X2Hotkey:SaveHotKey()
     end
     
